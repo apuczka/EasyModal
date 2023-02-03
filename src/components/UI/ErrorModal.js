4b12c982550401380acd6e311ba5192730014e20
apuczka <a.puczka@icloud.com>
2023-02-03 14:31:36 +0100

    add Portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,12 +1,45 @@
+// import React from "react";
+// import Button from "./Button";
+// import Card from "./Card";
+// import classes from "./ErrorModal.module.css";
+
+// const ErrorModal = (props) => {
+//   return (
+//     <div>
+//     <div  className={classes.backdrop} onClick={props.onConfirm} />
+//     <Card className={classes.modal}>
+//       <header className={classes.header}>
+//         <h2>{props.title}</h2>
+//       </header>
+//       <div className={classes.content}>
+//         <p>{props.message}</p>
+//       </div>
+//       <footer className={classes.actions}>
+//         <Button onClick={props.onConfirm}>Okay</Button>
+//       </footer>
+//     </Card>
+//     </div>
+//   );
+// };
+
+// export default ErrorModal;
+
+
+
+
+
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <div>
-    <div  className={classes.backdrop} onClick={props.onConfirm} />
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -18,8 +51,28 @@ const ErrorModal = (props) => {
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
-    </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
 export default ErrorModal;
+
+
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -44,7 +44,7 @@ const AddUsers = (props) => {
   };
   return (
     <Wrapper>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+      {error && (<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />)}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
